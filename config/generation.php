e63d73a10d5e5c788e467bf76e60f16d79a6bfaf
Swapnil Sarwe <swapnilsarwe@gmail.com>
2023-02-27 19:17:53 +0530

    added auto update generation config

--- config/generation.php
+++ config/generation.php
@@ -1,8 +1,40 @@
 <?php
 
+use Codeat3\BladeIconGeneration\IconProcessor;
+
+$svgNormalization = static function (string $tempFilepath, array $iconSet) {
+
+    // perform generic optimizations
+    $iconProcessor = new IconProcessor($tempFilepath, $iconSet);
+    $iconProcessor
+        ->optimize()
+        ->postOptimizationAsString(function ($svgLine){
+            // $svgLine = str_replace('stroke="#000"', 'stroke="currentColor"', $svgLine);
+            // return str_replace('fill="black"', 'fill="currentColor"', $svgLine);
+            $replacePattern = [
+                '/stroke="#[0-9A-Z]+"/s' => 'stroke="currentColor"',
+                '/fill="#[0-9A-Z]+"/s' => 'fill="currentColor"',
+            ];
+            return preg_replace(array_keys($replacePattern), array_values($replacePattern), $svgLine);
+        })
+        ->save();
+};
+
 return [
     [
-        'source' => null,
+        // Define a source directory for the sets like a node_modules/ or vendor/ directory...
+        'source' => __DIR__.'/../dist/public/img',
+
+        // Define a destination directory for your icons. The below is a good default...
         'destination' => __DIR__.'/../resources/svg',
+
+        // Enable "safe" mode which will prevent deletion of old icons...
+        'safe' => false,
+
+        // Call an optional callback to manipulate the icon
+        // with the pathname of the icon and the settings from above...
+        'after' => $svgNormalization,
+
+        'is-solid' => true,
     ],
 ];
